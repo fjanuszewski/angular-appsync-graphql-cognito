2f25ce2a84a8fc87d6cfe391d7061ca422cc5b77
fjanuszewski <fabian.januszewski@gmail.com>
2020-07-14 16:39:18 -0300

    add ssl cert in cloudfront

--- deploy.sh
+++ deploy.sh
@@ -7,34 +7,44 @@ NC='\033[0m' # No Color
 ENV=XXXXXX #THIS WORK FINE IF WE USE SAM IN LOCAL. IN PIPELINE IS NOT NEED
 BUCKET=XXXXXX #BUCKET IS REQUIRED FOR SAM PACKAGE
 
+DOMAIN=XXXXX
+SUBDOMAIN=XXXXX
+CERTARN=XXXXX
+
 STACK=XXXXXX-web-$ENV #NAME OF STACK, IS IMPORTANT FOR THE NAME OF ALL OBJECTS IN TEMPLATE
 PROJECT=XXXXXX #PROJECT NAME FOR THE TAGS
 
-echo -e "${YELLOW} Building SAM package and uploading cloudformation"
-echo -e " ================================================= ${NC}"
+AWS_PROFILE=XXXX
 
-sam package --template-file ./template.yaml --output-template-file packaged-template.yaml --s3-bucket $BUCKET
-sam deploy --template-file packaged-template.yaml --stack-name $STACK --tags Project=$PROJECT --parameter-overrides ProjectId=$PROJECT --capabilities CAPABILITY_NAMED_IAM
+echo "${YELLOW} Validating local SAM Template..."
+echo " ================================${NC}"
+sam validate --profile $AWS_PROFILE --template "template.yaml"
 
-echo -e "${YELLOW} Building frontend"
-echo -e " =================${NC}"
-npm run build --prod --prefix serverlesswebexample/
+echo "${YELLOW} Building SAM package and uploading cloudformation"
+echo " ================================================= ${NC}"
 
-echo -e "${YELLOW} Desrcibe Stack"
-echo -e " ===============${NC}"
+sam package --profile $AWS_PROFILE --template-file ./template.yaml --output-template-file packaged-template.yaml --s3-bucket $BUCKET
+sam deploy --profile $AWS_PROFILE --template-file packaged-template.yaml --stack-name $STACK --tags Project=$PROJECT --parameter-overrides CertArn=$CERTARN ProjectId=$PROJECT Domain=$DOMAIN SubDomain=$SUBDOMAIN --capabilities CAPABILITY_NAMED_IAM
 
-CLOUDFRONT_DISTRIBUTION=`aws cloudformation describe-stacks --stack-name "$STACK" --output text | grep $STACK-PortalDistribution | awk -F"\t" '{$0=$5}6'`
+
+echo "${YELLOW} Building frontend"
+echo " =================${NC}"
+npm install
+ng build --prod --aot --vendor-chunk --common-chunk --delete-output-path --buildOptimizer
+
+echo "${YELLOW} Desrcibe Stack"
+echo " ===============${NC}"
+
+CLOUDFRONT_DISTRIBUTION=`aws cloudformation describe-stacks --profile $AWS_PROFILE --stack-name "$STACK" --output text | grep $STACK-PortalDistribution | awk -F"\t" '{$0=$5}6'`
 # Deploy to AWS S3
 
-echo -e "${YELLOW} Deploy to S3 FE"
-echo -e " ===============${NC}"
+echo "${YELLOW} Deploy to S3 FE"
+echo " ===============${NC}"
 
-aws s3 cp serverlesswebexample/dist/serverlesswebexample/ s3://$STACK/  --acl public-read --recursive
+aws s3 sync dist/ s3://$STACK/  --acl public-read --profile $AWS_PROFILE
 # This is to set the index cache to 1 hour so when there are updates they don't take to long to reach all users
-aws s3 cp serverlesswebexample/dist/serverlesswebexample/index.html s3://$STACK/  --acl public-read --recursive --cache-control max-age=3600
+aws s3 sync dist/index.html s3://$STACK/  --acl public-read --profile $AWS_PROFILE --cache-control max-age=3600
 
-
-echo -e "${YELLOW} Creating Invalidation "
-echo -e " ===================== ${NC}"
-
-aws cloudfront create-invalidation --distribution-id $CLOUDFRONT_DISTRIBUTION --path "/*"
+echo "${YELLOW} Creating Invalidation "
+echo " ===================== ${NC}"
+aws cloudfront create-invalidation --profile $AWS_PROFILE --distribution-id $CLOUDFRONT_DISTRIBUTION --path "/*"
